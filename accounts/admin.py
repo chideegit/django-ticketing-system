e5b93d6accc5c1bef1dcd9839ffc922898c259c7
chidee <chidiebereohiri@gmail.com>
2023-12-02 00:04:24 +0100

    fixed the account/admin file

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        *UserAdmin.fieldsets, 
+        (
+            'Custom Field Heading', 
+            {
+                'fields': (
+                    'is_customer', 
+                    'is_engineer'
+                )
+            }
+        )
+    )
+
+admin.site.register(User, CustomUserAdmin)
